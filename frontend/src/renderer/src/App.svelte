699b1239b080fc3451dcfadb26d194df419b35d4
Jeroen Mols <jeroen@jeroenmols.com>
2024-06-28 23:59:11 +0200

    Electron-vite scaffolding of a Svelte application

---
+++ frontend/src/renderer/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { onMount } from 'svelte'
+  import ComingSoon from './components/ComingSoon.svelte'
+
+  let greeting = '...loading'
+
+  onMount(() => {
+    fetch('http://localhost:8080/hello/jeroen')
+      .then((response) => response.json())
+      .then((data) => {
+        greeting = data.greeting
+      })
+  })
+</script>
+
+<h1>{greeting}</h1>
+<ComingSoon />
---
+++ frontend/src/renderer/src/components/ComingSoon.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script>
+
+<p>🚧 Poolside tasks coming soon 🚧</p>
