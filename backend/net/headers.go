bd02ea3b643d3f281e9dbf64f07137064e46fff4
Jeroen Mols <jeroen@jeroenmols.com>
2024-06-29 14:59:17 +0200

    Add content-type header

--- backend/net/headers.go
+++ backend/net/headers.go
@@ -5,5 +5,6 @@ import "net/http"
 type AddResponseHeaders func(w http.ResponseWriter)
 
 func AddCorsReponseHeaders(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 }
