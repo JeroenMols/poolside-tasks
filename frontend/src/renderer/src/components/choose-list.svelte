59ec85474cf6f329df291f6fa9b670c72341bb2c
Jeroen Mols <jeroen@jeroenmols.com>
2024-06-30 18:10:30 +0200

    Allow frontend to join and create todo lists

--- frontend/src/renderer/src/components/choose-list.svelte
+++ frontend/src/renderer/src/components/choose-list.svelte
@@ -1,19 +1,52 @@
 <script lang="ts">
+  import { ensureNonEmpty } from '../utils/assertions'
+
   export let accessToken: string = ''
   export let onListSelected: (list: string) => void
+
+  let list_id = ''
+
+  type CreateTodoListResponse = {
+    todo_list_id: string
+  }
+
+  const createList = async () => {
+    let response = await fetch('http://localhost:8080/todolists', {
+      headers: { Authorization: `${accessToken}` },
+      method: 'POST',
+      body: '{}'
+    })
+
+    if (response.ok) {
+      const registerResponse = (await response.json()) as CreateTodoListResponse
+      ensureNonEmpty(registerResponse.todo_list_id)
+      onListSelected(registerResponse.todo_list_id)
+    } else {
+      alert(`Failed to create todo list - backend error (${response.status})`)
+    }
+  }
+
+  const checkListExists = async () => {
+    let response = await fetch(`http://localhost:8080/todolists/${list_id}`, {
+      headers: { Authorization: `${accessToken}` },
+      method: 'GET'
+    })
+
+    if (response.ok) {
+      onListSelected(list_id)
+    } else {
+      alert(`Failed to get todo list - backend error (${response.status})`)
+    }
+  }
 </script>
 
 <h1>TODO lists</h1>
 <p>Create or join a todo list</p>
-<input type="text" placeholder="Enter todolist id" />
-<button
-  on:click={() => {
-    onListSelected('fake_list_id')
-  }}>Join list</button
->
+<input type="text" bind:value={list_id} placeholder="Enter todolist id" />
+<button on:click={() => checkListExists()}>Join list</button>
 <p>Or</p>
 <button
   on:click={() => {
-    onListSelected('fake_list_id')
+    createList()
   }}>Create list</button
 >
